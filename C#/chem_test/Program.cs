59eb1cbc08c2ebec8be7e493dcb9d96861982202
dlabca <151052093+dlabca@users.noreply.github.com>
2024-09-15 15:16:12 +0200

    Update Program.cs

--- C#/chem_test/Program.cs
+++ C#/chem_test/Program.cs
@@ -1,2 +1,92 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+using System.Collections.Generic;
+
+class ChemickaZkouska
+{
+    static Dictionary<string, string> prvky = new Dictionary<string, string>
+    {
+        { "He", "Helium" },
+        { "Au", "Zlato" },
+        { "O", "Kyslik" },
+        { "C", "Uhlik" },
+        { "H", "Vodik" },
+        { "Li", "Lithium" },
+        { "As", "Arzen" },
+        { "U", "Uran" },
+        { "Zn", "Zinek" },
+        { "Pt", "Platina" }
+    };
+
+    static void Main(string[] args)
+    {
+        Console.Write("Kolik chcete otázek? ");
+        int pocetOtazek;
+
+        // Kontrola, aby uživatel zadal správný vstup
+        while (!int.TryParse(Console.ReadLine(), out pocetOtazek) || pocetOtazek <= 0)
+        {
+            Console.WriteLine("Zadejte prosím platné číslo otázek.");
+        }
+
+        Random rnd = new Random();
+        int spravne = 0;
+        string predchoziOdpoved = "";
+
+        for (int i = 0; i < pocetOtazek; i++)
+        {
+            Console.Clear();  // Vymaže konzoli po každé otázce
+            Console.WriteLine(i + "/" + pocetOtazek);
+            if (predchoziOdpoved != "")
+                Console.WriteLine("predchozi odpoved byla " + predchoziOdpoved);
+            int typOtazky = rnd.Next(2); // 0 = chemická značka -> český název, 1 = český název -> chemická značka
+            int index = rnd.Next(prvky.Count);
+            KeyValuePair<string, string> prvek = new KeyValuePair<string, string>();
+
+            foreach (var item in prvky)
+            {
+                if (index-- == 0)
+                {
+                    prvek = item;
+                    break;
+                }
+            }
+
+            if (typOtazky == 0)
+            {
+                Console.Write($"Jaký je český název prvku s chemickou značkou {prvek.Key}? ");
+                string odpoved = Console.ReadLine();
+                if (odpoved.Equals(prvek.Value, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("Správně!");
+                    spravne++;
+                    predchoziOdpoved = "Spravne!";
+                }
+                else
+                {
+                    Console.WriteLine($"Špatně. Správná odpověď je {prvek.Value}.");
+                    predchoziOdpoved = "Spatne!";
+                }
+            }
+            else
+            {
+                Console.Write($"Jaká je chemická značka pro {prvek.Value}? ");
+                string odpoved = Console.ReadLine();
+                if (odpoved.Equals(prvek.Key, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("Správně!");
+                    spravne++;
+                    predchoziOdpoved = "Spravne!";
+                }
+                else
+                {
+                    Console.WriteLine($"Špatně. Správná odpověď je {prvek.Key}.");
+                    predchoziOdpoved = "Spatne!";
+                }
+            }
+        }
+
+        Console.Clear();
+        Console.WriteLine($"\nKonec testu. Počet správných odpovědí: {spravne}/{pocetOtazek}.");
+    }
+}
