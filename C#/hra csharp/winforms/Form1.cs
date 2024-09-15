b9634a1c7c2f23d0e3820960410a2eb76db2af24
dlabca <dlabca.dlabca@gmail.com>
2024-09-15 15:13:38 +0200

    nwm

---
+++ C#/hra csharp/winforms/Form1.cs
@@ -0,0 +1,9 @@
+namespace winform;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+}
---
+++ C#/matematika/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace ArithmeticPracticeGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int num1, num2, correctAnswer, userAnswer;
+
+            Console.WriteLine("Welcome to the Arithmetic Practice Game!");
+            Console.WriteLine("Enter your answers and press Enter to submit.");
+
+            while (true)
+            {
+                // Generate two random numbers for the arithmetic operation
+                num1 = random.Next(1, 10);
+                num2 = random.Next(1, 10);
+
+                // Choose a random arithmetic operation (+, -, *, /)
+                int operation = random.Next(1, 4);
+                string operationSymbol = "";
+                switch (operation)
+                {
+                    case 1:
+                        operationSymbol = "+";
+                        correctAnswer = num1 + num2;
+                        break;
+                    case 2:
+                        operationSymbol = "-";
+                        correctAnswer = num1 - num2;
+                        break;
+                    case 3:
+                        operationSymbol = "*";
+                        correctAnswer = num1 * num2;
+                        break;
+                    case 4:
+                        operationSymbol = "/";
+                        correctAnswer = num1 / num2;
+                        break;
+                }
+
+                // Display the arithmetic problem
+                Console.Write($"{num1} {operationSymbol} {num2} = ");
+                string userInput = Console.ReadLine();
+
+                // Try to parse the user's input
+                if (int.TryParse(userInput, out userAnswer))
+                {
+                    if (userAnswer == correctAnswer)
+                    {
+                        Console.WriteLine("Correct!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Sorry, the correct answer is {correctAnswer}.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a whole number.");
+                }
+            }
+        }
+    }
+}
